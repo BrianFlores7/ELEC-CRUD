6014335cb5cbed6da2c1167cebace54df860e0cc
Brian <brianbtfa@hotmail.com>
2021-04-22 18:56:45 -0500

    crud votantes added

---
+++ crudVotantes.js
@@ -0,0 +1,21 @@
+let getData = function()
+{
+    var sentencia = 'SELECT "NOMBRE", "APELLIDO", "CURP", "CLAVEELECTOR" FROM "PQG82494"."VOTANTES";'
+    return sentencia
+}
+
+let setData = function(nombre, apellido, curp, clave_elector)
+{
+    var sentencia = 'INSERT INTO  "PQG82494"."VOTANTES" '
+    +  '("NOMBRE","APELLIDO","CURP","CLAVEELECTOR") VALUES('
+    + "'" + nombre + "','" + apellido + "','" + curp + "','" + clave_elector + "');" 
+    return sentencia
+}
+
+let getData = function(curp)
+{
+    var sentencia = 'SELECT "NOMBRE", "APELLIDO", "CURP", "CLAVEELECTOR" FROM "PQG82494"."VOTANTES" WHERE "CURP" = '
+    + "'" + curp + "'"
+    return sentencia
+}
+
