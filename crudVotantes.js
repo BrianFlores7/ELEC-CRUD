8ba3d55b72b600b2c8625b3cf64031c568fc4425
Brian <brianbtfa@hotmail.com>
2021-04-28 21:36:30 -0500

    municipio y estado added

--- crudVotantes.js
+++ crudVotantes.js
@@ -4,17 +4,17 @@ let get_Data = function()
     return sentencia
 }
 
-let setData = function(nombre, apellido, curp, clave_elector)
+let setData = function(nombre, apellido, curp, clave_elector, estado, municipio)
 {
     var sentencia = 'INSERT INTO  "PQG82494"."VOTANTES" '
-    +  '("NOMBRE","APELLIDO","CURP","CLAVEELECTOR") VALUES('
-    + "'" + nombre + "','" + apellido + "','" + curp + "','" + clave_elector + "');" 
+    +  '("NOMBRE","APELLIDO","CURP","CLAVEELECTOR", "ESTADO", "MUNICIPIO") VALUES('
+    + "'" + nombre + "','" + apellido + "','" + curp + "','" + clave_elector + "', '"+ estado + "', '" + municipio + "');" 
     return sentencia
 }
 
 let getData = function(curp)
 {
-    var sentencia = 'SELECT "NOMBRE", "APELLIDO", "CURP", "CLAVEELCTOR" FROM "PQG82494"."VOTANTES" WHERE "CURP" = '
+    var sentencia = 'SELECT "NOMBRE", "APELLIDO", "CURP", "CLAVEELCTOR", "ESTADO", "MUNICIPIO" FROM "PQG82494"."VOTANTES" WHERE "CURP" = '
     + "'" + curp + "'"
     return sentencia
 }
